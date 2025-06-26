a951f5972c102a666ceda17a7d84ff281c81192d
ABHINAV PAL <114683444+abhinavpal2405@users.noreply.github.com>
2025-06-26 09:03:06 +0530

    Time: 8 ms (29.28%), Space: 41.4 MB (99.1%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+class Solution {
+    public int longestPalindrome(String s) {
+        if (s.length() == 1) {
+            return 1;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int ans = 0;
+        int odd = 0;
+        for (char key : map.keySet()) {
+            int freq = map.get(key);
+            if (freq % 2 == 0) {
+                ans += freq;
+            } else {
+                ans += freq - 1;
+                odd++;
+            }
+        }
+
+        if (odd > 0) {
+            ans += 1; 
+        }
+
+        return ans;
+    }
+}
