7327ad405e8287ee542c66d5ac647d2c909fef4b
ABHINAV PAL <114683444+abhinavpal2405@users.noreply.github.com>
2025-07-11 21:45:32 +0530

    Time: 1 ms (100%), Space: 45.6 MB (62.59%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
@@ -1,21 +1,32 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int n =grid.length;
-        Set<Integer> set=new HashSet<>();
-        int a=0,b=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(set.contains(grid[i][j])){
-                    a=grid[i][j];
+        int n = grid.length;
+        int[] ans = new int[2];
+
+        // First iteration: mark visited cells using negative sign
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int cur = Math.abs(grid[i][j]);
+                int row = (cur - 1) / n;
+                int col = (cur - 1) % n;
+
+                if (grid[row][col] < 0) {
+                    ans[0] = cur;  // Repeated value found
+                } else {
+                    grid[row][col] *= -1;
                 }
-                set.add(grid[i][j]);
             }
         }
-        for(int i =1;i<=n*n;i++){
-            if(!set.contains(i)){
-                b=i;
+
+        // Second iteration: find missing value (positive value left)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] > 0) {
+                    ans[1] = i * n + j + 1;  // Missing value
+                }
             }
         }
-        return new int[]{a,b};
+
+        return ans;
     }
 }
