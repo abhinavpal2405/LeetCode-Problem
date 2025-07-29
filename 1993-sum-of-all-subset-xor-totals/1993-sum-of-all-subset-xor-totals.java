264a63d6fe4ab6a21693c4cc02f938adaa5cb0e6
ABHINAV PAL <114683444+abhinavpal2405@users.noreply.github.com>
2025-07-29 14:51:19 +0530

    Time: 0 ms (100%), Space: 41 MB (52.86%) - LeetHub

---
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int count=0;
+        for(int num:nums){
+            count |= num;
+            }
+         return count *(1<< (nums.length-1));
+    }
+    
+}
