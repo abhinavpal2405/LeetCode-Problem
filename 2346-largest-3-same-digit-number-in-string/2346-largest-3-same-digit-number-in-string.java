e33529da353b943c61dcdda54d2afd91a00605b8
ABHINAV PAL <114683444+abhinavpal2405@users.noreply.github.com>
2025-08-14 22:36:34 +0530

    Time: 9 ms (16.18%), Space: 41.7 MB (92.25%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
@@ -2,10 +2,10 @@ class Solution {
     public String largestGoodInteger(String num) {
         String s = "";
         for (int i = 0; i < num.length(); i++) {
-            if (num.contains("999") || num.contains("888") || num.contains("777") ||
-                num.contains("666") || num.contains("555") || num.contains("444") ||
-                num.contains("333") || num.contains("222") || num.contains("111") ||
-                num.contains("000")) {
+            // if (num.contains("999") || num.contains("888") || num.contains("777") ||
+            //     num.contains("666") || num.contains("555") || num.contains("444") ||
+            //     num.contains("333") || num.contains("222") || num.contains("111") ||
+            //     num.contains("000")) {
                 if (num.contains("999")) return "999";
                 if (num.contains("888")) return "888";
                 if (num.contains("777")) return "777";
@@ -16,7 +16,7 @@ class Solution {
                 if (num.contains("222")) return "222";
                 if (num.contains("111")) return "111";
                 if (num.contains("000")) return "000";
-            }
+            
         }
         return s;
     }
