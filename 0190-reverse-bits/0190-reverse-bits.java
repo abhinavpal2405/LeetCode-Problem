7e11184e02d9e12dd91d7a3059e5b9a7e07f9bbb
ABHINAV PAL <114683444+abhinavpal2405@users.noreply.github.com>
2026-01-08 11:39:28 +0530

    Time: 0 ms (100%), Space: 42.6 MB (36.15%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,13 +1,11 @@
 class Solution {
     public int reverseBits(int n) {
-        int mask=1;
-        int ans=0;
-        for(int i=0;i<32;i++){
-            if((n&mask)!=0){
-                ans +=1<<31-i;
-            }
-            mask<<=1;
+    int result =0;
+        for(int i =0;i<32;i++){
+            result <<=1;
+            result |= (n&1);
+            n >>= 1;
         }
-        return ans;
+        return result;
     }
 }
